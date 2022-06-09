a3c3f96c90c0a2478185e1d7fe33ddba5aca7779
Vinogradov, Alexei <alexei.vinogradov@accenture.com>
2022-06-09 18:30:36 +0200

    first lecture

--- src/test/java/github/SelenideRepositoryTest.java
+++ src/test/java/github/SelenideRepositoryTest.java
@@ -10,18 +10,11 @@ public class SelenideRepositoryTest {
 
   @Test
   void shouldFindSelenideAsFirstRepository(){
+    // region с чего начинаем?
     // открыть страницу github.com
-    open("https://github.com");
     // ввести в поле поиска selenide и нажать Enter
-    $("[data-test-selector=nav-search-input]").setValue("selenide").pressEnter();
-
     // нажимаем на линк от первого результата поиска
-    $$("ul.repo-list li").first().$("a").click();
     // check: в заголовке встречается selenide/selenide
-    $("h2").shouldHave(text("selenide / selenide"));
-
-
-
-
+    // endregion
   }
 }
